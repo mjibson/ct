4ad3eb02fd312bf50121dc1ba224e130e1351983
Matt Jibson <matt.jibson@gmail.com>
2018-12-03 00:54:14 -0500

    initial

---
+++ dl.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/google/go-cmp/cmp"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	fmt.Println("err:", dl())
+}
+
+func dl() error {
+	//const wiki_url = "https://en.wikipedia.org/wiki/List_of_cocktails"
+	const wiki_url = "https://en.wikipedia.org/wiki/List_of_IBA_official_cocktails"
+	doc, err := goquery.NewDocument(wiki_url)
+	if err != nil {
+		return errors.Wrap(err, "download main")
+	}
+	urlch := make(chan string)
+	go func() {
+		defer close(urlch)
+		seen := map[string]bool{}
+		doc.Find("#mw-content-text .div-col a[title]").Each(func(i int, s *goquery.Selection) {
+			n := s.Get(0)
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					u := "https://en.wikipedia.org" + a.Val
+					if !seen[u] {
+						seen[u] = true
+						urlch <- u
+					}
+				}
+			}
+		})
+	}()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	ctch := make(chan CT)
+	go func() {
+		defer close(ctch)
+		defer wg.Done()
+		for u := range urlch {
+			doc, err := goquery.NewDocument(u)
+			if err != nil {
+				panic(err)
+			}
+			doc.Find("table.infobox").Each(func(i int, s *goquery.Selection) {
+				cap := s.Find("* > caption").Text()
+				if cap == "" {
+					return
+				}
+				var greds, short []string
+				var details [][]string
+				s.Find("tbody > tr").Each(func(i int, tr *goquery.Selection) {
+					th := strings.ToLower(strings.TrimSpace(tr.Find("th").Text()))
+					td := strings.TrimSpace(tr.Find("td").Text())
+					if th == "" || td == "" {
+						return
+					}
+					if strings.Contains(th, "ingredients") {
+						if len(greds) != 0 {
+							panic(u)
+						}
+						tr.Find("td > ul > li").Each(func(i int, li *goquery.Selection) {
+							g := strings.TrimSpace(li.Text())
+							if g != "" {
+								greds = append(greds, g)
+							}
+						})
+						short = make([]string, len(greds))
+						for i, s := range greds {
+							for {
+								s = strings.ToLower(strings.TrimSpace(strings.TrimLeft(s, "+–0123456789./½¾⅔")))
+								s = strings.TrimRight(s, ".")
+								changed := false
+								for _, m := range measurements {
+									m += " "
+									if strings.HasPrefix(strings.ToLower(s), m) {
+										s = strings.TrimSpace(s[len(m):])
+										changed = true
+									}
+									if strings.HasPrefix(s, "of ") {
+										s = s[3:]
+										changed = true
+									}
+								}
+								ns := strings.Join(strings.Fields(strings.Join(parenRE.Split(s, -1), " ")), " ")
+								if ns != s {
+									changed = true
+									s = ns
+								}
+								if !changed {
+									break
+								}
+							}
+							if s == "" {
+								fmt.Println("no short", greds[i], cap, u)
+							}
+							short[i] = s
+						}
+						return
+					}
+					details = append(details, []string{th, td})
+				})
+				if len(greds) == 0 {
+					return
+				}
+				ct := CT{
+					Name:       cap,
+					Link:       u,
+					Details:    details,
+					Greds:      greds,
+					ShortGreds: short,
+				}
+				ctch <- ct
+			})
+		}
+	}()
+	cts := map[string]CT{}
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			seen := map[string]bool{}
+			for ct := range ctch {
+				if prev, ok := cts[ct.Name]; ok {
+					if !cmp.Equal(ct.Greds, prev.Greds) {
+						fmt.Println("unmatching", ct.Name, prev.Link, ct.Link)
+					}
+					continue
+				}
+				cts[ct.Name] = ct
+				for _, s := range ct.ShortGreds {
+					s = strings.ToLower(s)
+					if !seen[s] {
+						//fmt.Println(s, ct.Link, ct.Name)
+						seen[s] = true
+					}
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	b, _ := json.MarshalIndent(cts, "", "  ")
+	fmt.Println(string(b))
+
+	return ioutil.WriteFile("src/drinks.json", b, 0666)
+}
+
+var (
+	measurements = []string{
+		"one",
+		"two",
+		"three",
+
+		"cl",
+		"ml",
+		"dashes",
+		"dash",
+		"drops",
+		"each",
+		"fresh",
+		"freshly squeezed",
+		"oz.",
+		"oz",
+		"ounces",
+		"ounce",
+		"parts",
+		"part",
+		"pint",
+		"shot",
+		"slice",
+		"tbsp",
+		"teaspoons",
+		"teaspoon",
+		"top with",
+		"us fluid ounces",
+		"us fluid ounce",
+		"cpu",
+		"sprig",
+		"measure",
+		"tsp",
+		"to",
+		"garnish",
+		"a splash",
+		"splash",
+		"or",
+		"few",
+		"a pinch",
+		"pinch",
+	}
+	parenRE = regexp.MustCompile(`\(.*?\)`)
+)
+
+type CT struct {
+	Name       string
+	Link       string
+	Details    [][]string
+	Greds      []string
+	ShortGreds []string
+}
--- src/App.js
+++ src/App.js
@@ -1,28 +1,131 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'tachyons/css/tachyons.min.css';
 import './App.css';
+import drinks from './drinks';
+
+const allGreds = {};
+Object.values(drinks).forEach(v =>
+	v.ShortGreds.forEach(g => {
+		allGreds[g] = (allGreds[g] || 0) + 1;
+	})
+);
+
+const colClass = 'w-25';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		const gredsByCount = Object.keys(allGreds);
+		gredsByCount
+			.sort((a, b) => {
+				return allGreds[a] - allGreds[b];
+			})
+			.reverse();
+		super(props);
+		const have = JSON.parse(localStorage.getItem('have') || '{}');
+		this.state = {
+			allGreds: allGreds,
+			gredsByCount: gredsByCount,
+			have: have,
+			make: [],
+			could: [],
+		};
+	}
+	componentDidMount() {
+		this.update();
+	}
+	update = () => {
+		const make = [];
+		const could = [];
+		Object.values(drinks).forEach(drink => {
+			const missing = [];
+			drink.ShortGreds.forEach(v => {
+				if (!this.state.have[v]) {
+					missing.push(v);
+				}
+			});
+			if (!missing.length) {
+				make.push(drink);
+			} else if (
+				missing.length <= 2 &&
+				drink.ShortGreds.length > missing.length
+			) {
+				could.push(Object.assign({ missing: missing }, drink));
+			}
+		});
+		make.sort((a, b) => a.Name.localeCompare(b.Name));
+		could.sort((a, b) => {
+			if (a.missing.length !== b.missing.length) {
+				return b.missing.length - a.missing.length;
+			}
+			return a.Name.localeCompare(b.Name);
+		});
+		this.setState({ make: make, could: could });
+	};
+	clickGred = v => {
+		const have = this.state.have;
+		if (v.target.checked) {
+			have[v.target.name] = true;
+		} else {
+			delete have[v.target.name];
+		}
+		localStorage.setItem('have', JSON.stringify(have));
+		this.setState({ have: have }, this.update);
+	};
+	render() {
+		const have = Object.keys(this.state.have)
+			.sort()
+			.map(v => (
+				<li className="ma1" key={v}>
+					<label>
+						{v} ({this.state.allGreds[v]})
+					</label>
+				</li>
+			));
+		const greds = this.state.gredsByCount.map(v => (
+			<li className="ma1" key={v}>
+				<label>
+					<input
+						type="checkbox"
+						name={v}
+						onChange={this.clickGred}
+						checked={!!this.state.have[v]}
+					/>{' '}
+					{v} ({this.state.allGreds[v]})
+				</label>
+			</li>
+		));
+		const make = this.state.make.map(v => (
+			<div key={v.Name} className="ma2">
+				<a href={v.Link}>{v.Name}</a>: {v.ShortGreds.join(', ')}
+			</div>
+		));
+		const could = this.state.could.map(v => (
+			<div key={v.Name} className="ma2">
+				<a href={v.Link}>{v.Name}</a> ({v.missing.length}
+				): {v.missing.join(', ')}
+			</div>
+		));
+		return (
+			<div className="sans-serif ma3 flex">
+				<div className={colClass}>
+					possible ingredients:
+					<ul className="list">{greds}</ul>
+				</div>
+				<div className={colClass}>
+					have:
+					<ul className="list">{have}</ul>
+				</div>
+				<div className={colClass}>
+					can make:
+					<ul className="list indent-n2em">{make}</ul>
+				</div>
+				<div className={colClass}>
+					could make (missing ingredients shown):
+					<ul className="list">{could}</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
